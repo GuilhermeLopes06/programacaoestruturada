73ab181c88fa1d86f77d5b8bc940d24ca39bd493
GuilhermeLopes06 <160645513+GuilhermeLopes06@users.noreply.github.com>
2024-02-28 20:51:12 -0300

    Delete 02-AC_bissexto.py

--- 02-AC_bissexto.py
+++
@@ -1,2 +0,0 @@
-ano = int(input("Informe o ano: "))
-print(ano%4 == 0 and ano%100 != 0 or ano%400 == 0 and ano%100 == 0) #Dessa forma são separados os anos bissextos múltiplos e não múltiplos de 100
