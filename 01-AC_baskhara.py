4bddb989551acf539ff4a7556407f0e9c3729ae1
GuilhermeLopes06 <160645513+GuilhermeLopes06@users.noreply.github.com>
2024-02-28 20:51:21 -0300

    Delete 01-AC_baskhara.py

--- 01-AC_baskhara.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-a = int(input("Informe o parâmetro a da equação: "))
-b = int(input("Informe o parâmetro b da equação: "))
-c = int(input("Informe o parâmetro c da equação: "))
-delta = b**2 - 4*a*c 
-raiz1 = (-b + delta**0.5)/2**a 
-raiz2 = (-b - delta**0.5)/2**a 
-print("A primeira raíz da equação é ",raiz1)
-print("A segunda raíz da equação é ",raiz2)
