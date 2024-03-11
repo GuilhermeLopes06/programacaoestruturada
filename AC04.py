927ecb54f13e43ef915a946d57116c67c540297e
GuilhermeLopes06 <160645513+GuilhermeLopes06@users.noreply.github.com>
2024-03-11 15:24:13 -0300

    Add files via upload

--- AC04.py
+++ AC04.py
@@ -3,7 +3,7 @@ def ler_nome():
     return input("Qual é o seu nome? ")
 
 def ler_notas():
-    """Retorna as notas"""
+    """Le e retorna as notas"""
     ap1 = float(input("Nota da AP1: "))
     ap2 = float(input("Nota da AP2: "))
     asub = float(input("Nota da AS: "))
@@ -12,8 +12,9 @@ def ler_notas():
 
 
 def notas_sao_validas(ap1,ap2,asub,ac):
-    """Retorna Tre se as notas rtão entre 0 e 10
-        Retorna False caso cotrário
+    """
+    Retorna Tre se as notas rtão entre 0 e 10
+    Retorna False caso cotrário
     """
     if 10 >= ap1 >= 0 and 10 >= ap2 >= 0 and 10 >= asub >= 0 and 10 >= ac >= 0:
         return True
@@ -22,7 +23,9 @@ def notas_sao_validas(ap1,ap2,asub,ac):
 
 
 def duas_maiores_notas(ap1,ap2,asub):
-    """Retorna as duas maiores notas dentre as avaliações
+    """
+    Retorna as duas maiores notas dentre as avaliações:
+    
     """
     if ap1 < asub and ap1 < ap2:
         return  asub,  ap2
@@ -37,9 +40,12 @@ def calcular_media(prova1,prova2,ac):
     """Retorna a média das avalições, arredondando para duas casas decimais
     MEDIA=(N1 + N2) * 0,4 + AC * 0,2
     """
-    return (prova1 + prova2) * 0.4 + ac * 0.2
+    return round((prova1 + prova2) * 0.4 + ac * 0.2, 2)
 
 def aluno_foi_aprovado(media):
+    """
+    Retorna True se a media for maior ou igual a 7, caso contrário retrona False
+    """
     return media >= 7
 
 
@@ -49,7 +55,7 @@ def main():
         ap1, ap2, asub, ac = ler_notas()
         if notas_sao_validas(ap1,ap2,asub,ac):
             prova1, prova2 = duas_maiores_notas(ap1, ap2, asub)
-            media = round(calcular_media(prova1,prova2,ac), 2)
+            media = calcular_media(prova1,prova2,ac)
             if aluno_foi_aprovado(media):
                 print("Aluno foi aprovado!", media)
             else:
