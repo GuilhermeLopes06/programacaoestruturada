47b58e0c6215634ed60da6feea6fcb9afa6aec5c
GuilhermeLopes06 <160645513+GuilhermeLopes06@users.noreply.github.com>
2024-05-06 15:06:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-"""
-SRP -> Single Responsibility Principle
-"""
-from cli import calculadora
-def main():
-    calculadora.iniciar()
+from conta import Conta
 
-if __name__ == "__main__":
-    main()
+c1 = Conta(1234, "João")
+c2 = Conta(5678, "Maria")
+
+print(c1.titular)
+print(c2.saldo)
+
+c1.depositar(50)
+
+print(c1.saldo)
+print(c2.saldo)
+
+c1.sacar(20)
+tenta_sacar = c2.sacar(20)
+
+if tenta_sacar:
+    print("Saque feito com sucesso!")
+else:
+    print("Saldo insuficiente")
+
+print(str(c1))
